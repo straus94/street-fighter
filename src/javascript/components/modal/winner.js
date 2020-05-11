9d43a4b6b74faa858cc94195cd327845a97ccbd4
Denys Herashchenko <straus94@gmail.com>
2020-05-11 21:53:43 +0300

    done project

---
+++ src/javascript/components/modal/winner.js
@@ -0,0 +1,10 @@
+import { showModal } from "./modal";
+
+export function showWinnerModal(fighter) {
+  const winnerInfo = {
+    title: '...And The WINNER!',
+    bodyElement: fighter.name
+  }
+  
+  showModal(winnerInfo);
+}
